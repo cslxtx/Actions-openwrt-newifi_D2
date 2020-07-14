9002e4ea1ce45822dc54c5d0c0ec306b55f616dd
cslxtx <lixionghui2015@gmail.com>
2020-07-14 17:02:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/openwrt-packages.git package/
-git clone https://github.com/kenzok8/small.git package/
-git clone https://github.com/tty228/luci-app-serverchan.git package/lean/
