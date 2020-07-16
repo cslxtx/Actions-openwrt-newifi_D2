39f56fdf95c097438e09c8521c24b0bf8921fb41
cslxtx <lixionghui2015@gmail.com>
2020-07-16 11:07:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+
+# 拉取微信推送、PPTP服务器插件
+svn co https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
+svn co https://github.com/xkstudio/luci-app-pptp-server package/lean/luci-app-pptp-server
