b27d2d564598be965ded258ab3ab4243b03e7521
H.Lee <lixionghui2015@gmail.com>
2020-07-16 20:23:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 
 # 拉取微信推送、PPTP服务器插件
 svn co https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
-svn co https://github.com/xkstudio/luci-app-pptp-server package/lean/luci-app-pptp-server
+svn co https://github.com/cslxtx/luci-app-pptp-vpnserver-manyusers.git package/lean/luci-app-pptp-vpnserver-manyusers
