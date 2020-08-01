12fc3e8d05794962abd9fadfc0af3e9be4a479c0
H.Lee <li3873@qq.com>
2020-08-01 16:27:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,9 +10,12 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+# 删除老argon
+rm -rf package/lean/luci-theme-argon
+# 拉取argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# 拉取微信推送、PPTP服务器、京东签到插件
-#svn co https://github.com/jerrykuku/node-request /package/lean/node-request
-#svn co https://github.com/jerrykuku/luci-app-jd-dailybonus /package/lean/luci-app-jd-dailybonus
-svn co https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
-svn co https://github.com/xkstudio/luci-app-pptp-server package/lean/luci-app-pptp-server
+# 拉取微信推送、京东签到插件
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
